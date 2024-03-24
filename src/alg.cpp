9fefb06de9719a9de885c53e192c0587fac0762d
vladudin1 <160415615+vladudin1@users.noreply.github.com>
2024-03-24 10:56:05 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,6 +1,29 @@
 // Copyright 2021 NNTU-CS
 
-int cbinsearch(int *arr, int size, int value) {
-  //  поместить сюда реализацию алгоритма
-  return 0; // если ничего не найдено
+int cbinsearch(int* arr, int size, int value) {
+    int left = 0;
+    int right = size - 1;
+    int count = 0;
+    while (left <= right) {
+        int middle = left + (right - left) / 2;
+        if (arr[middle] == value) {
+            count++;
+            int i = middle - 1;
+            while (i >= 0 && arr[i] == value) {
+                count++;
+                i--;
+            }
+            i = middle + 1;
+            while (i < size && arr[i] == value) {
+                count++;
+                i++;
+            }
+            return count;
+        } else if (arr[middle] < value) {
+            left = middle + 1;
+        } else {
+            right = middle - 1;
+        }
+    }
+    return count;
 }
